aa6d4ca58effd2728d0a2f9f13cc99bdcb0dc45e
metanugget <alex@hodge.link>
2023-05-06 11:10:13 -0400

    moved count read inside of interaction execute

--- commands/actions/nutcount.js
+++ commands/actions/nutcount.js
@@ -10,14 +10,14 @@ module.exports = {
     .setName('nutcount')
     .setDescription('Displays the number of times users have nutted in the server'),
   async execute(interaction) {
-    await interaction.reply(`Users have nutted ${count} time(s) in ${interaction.guild.name}.`);
+    fs.readFile('./data/count.txt', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        count = parseInt(data.toString());
+        const volumeresult = 2.5 * count;
+        interaction.reply(`Users have nutted ${count} time(s) in ${interaction.guild.name}. This is approximately ${volumeresult} ml of cum.`);
+      }
+    });
   },
 };
-
-fs.readFile('./data/count.txt', (err, data) => {
-  if (err) {
-    console.error(err);
-  } else {
-    count = parseInt(data.toString());
-  }
-});
