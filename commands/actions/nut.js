94624951db2e4eb2f8f2d9c1abda2159da7f2a63
metanugget <alex@hodge.link>
2023-05-06 10:38:34 -0400

    Switched to SQLite to store count

--- commands/actions/nut.js
+++ commands/actions/nut.js
@@ -1,8 +1,8 @@
 // Increments nut counter and displays count
 
 const fs = require('fs');
-
-let count = 0;
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./db/database.sqlite');
 
 const { SlashCommandBuilder } = require('discord.js');
 
@@ -11,22 +11,20 @@ module.exports = {
     .setName('nutted')
     .setDescription('Counts how many times users have nutted here'),
   async execute(interaction) {
-    count++;
-    fs.writeFile('./data/count.txt', count.toString(), (err) => {
+    const countquery = 'SELECT MAX(nutcount) AS maxinteger FROM nuts';
+    db.get(countquery, (err, row) => {
       if (err) {
-        console.error(err);
+        console.error(err.message);
+        return;
       }
+      const count = row.maxinteger;
+      db.get(`INSERT INTO nuts (username, guildid, nutcount) VALUES ('${interaction.user.username}', '${interaction.guild.id}', ${count + 1})`, (err) => {
+        if (err) {
+          console.error(err.message);
+          return;
+        }
+        interaction.reply(`Nice, ${interaction.user.username}. 😏 Users have nutted ${count + 1} time(s) in ${interaction.guild.name}.`);
+      });
     });
-
-    await interaction.reply(`Nice, ${interaction.user.username}. 😏 Users have nutted ${count} time(s) in ${interaction.guild.name}.`);
   },
 };
-
-// Read the count from the file on startup
-fs.readFile('./data/count.txt', (err, data) => {
-  if (err) {
-    console.error(err);
-  } else {
-    count = parseInt(data.toString());
-  }
-});
--- commands/actions/nutcount.js
+++ commands/actions/nutcount.js
@@ -2,22 +2,23 @@
 
 const fs = require('fs');
 const { SlashCommandBuilder } = require('discord.js');
-
-let count = 0;
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./db/database.sqlite');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('nutcount')
     .setDescription('Displays the number of times users have nutted in the server'),
   async execute(interaction) {
-    await interaction.reply(`Users have nutted ${count} time(s) in ${interaction.guild.name}.`);
+    const countquery = 'SELECT MAX(nutcount) AS maxinteger FROM nuts';
+    db.get(countquery, (err, row) => {
+      if (err) {
+        console.error(err.message);
+        return;
+      }
+      const count = row.maxinteger;
+
+      interaction.reply(`Users have nutted ${count} time(s) in ${interaction.guild.name}.`);
+    });
   },
 };
-
-fs.readFile('./data/count.txt', (err, data) => {
-  if (err) {
-    console.error(err);
-  } else {
-    count = parseInt(data.toString());
-  }
-});
